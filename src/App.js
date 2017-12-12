ee6785ad58ab22dfae49b9245f32ca306a0a3984
383455526@qq.com <383455526@qq.com>
2017-12-12 15:21:36 +0800

    1、优化初始化样式 2、添加即时搜索,搜索更快速；搜索关键词优化，搜索更准确

--- src/App.js
+++ src/App.js
@@ -11,48 +11,62 @@ const Search = Input.Search;
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      searchResult:[],
-      initialDone:false
+    this.state = {
+      searchResult: [],
+      initialDone: false
     };
-    
+
     //获取api信息
-    Promise.all(apiCenter.collectApiInfo(config.getProjectUrls())).then(()=>{
-      this.setState({initialDone:true});
-      console.log("done");
-      console.dir(this)
-    })
+    Promise
+      .all(apiCenter.collectApiInfo(config.getProjectUrls()))
+      .then(() => {
+        this.setState({initialDone: true});
+        console.log("done");
+        console.dir(this)
+      })
   }
 
-
   render() {
+
+    let placeholder = "系统初始化中，请稍候...",
+        disabled = true;
+    if (this.state.initialDone) {
+      placeholder = "API名称/开发者姓名(pingyin)";
+      disabled = false;
+    }
     return (
-      
+
       <div className="App">
         <div className="search-wrapper">
           <img src="images/api_logo.png" className="logo"/>
           <Search
-            placeholder="API名称/开发者姓名(pingyin)"
+            placeholder={placeholder}
+            disabled={disabled}
             className="search"
             onInput={this.searchApi}
             onSearch={this.searchApi}
             size="large"
-            style={{visibility:this.state.initialDone?"visible":"hidden"}}
             type="primary"/>
         </div>
 
         <div className="main-content">
           <ApiList className="block list" dataList={this.state.searchResult}></ApiList>
           <div className="block nav">
-            <h3 style={{borderBottom:"1px solid #e1e1e1",padding:"5px"}}>快捷方式</h3>
+            <h3
+              style={{
+              borderBottom: "1px solid #e1e1e1",
+              padding: "5px"
+            }}>快捷方式</h3>
             <ul>
-              <li><a href="http://58.67.206.4:48080/" target="_blank">1、官方API目录</a></li>
+              <li>
+                <a href="http://58.67.206.4:48080/" target="_blank">1、Java API文档目录</a>
+              </li>
               <li>todo</li>
               <li>todo</li>
               <li>todo</li>
             </ul>
           </div>
-        </div>        
+        </div>
         <History onHistorySelect={this.searchApi}></History>
 
       </div>
@@ -60,16 +74,14 @@ class App extends Component {
   }
 
   searchApi = (key) => {
-    if(typeof key !="string"){
+    if (typeof key != "string") {
       key = key.target.value
     }
-    console.log(key);
     this.setState({
       searchResult: apiCenter.searchApi(key.toLowerCase())
     })
   }
 
-  
 }
 
 export default App;
